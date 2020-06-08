7b15c8f2182a84f1a93584271eb26b682bdb7960
xellDart <mundo2010@gmail.com>
2020-06-08 14:08:27 -0500

    Add metamask_button

---
+++ src/components/metamask_button.svelte
@@ -0,0 +1,5 @@
+<div style="border-radius: .2rem; background-color: #252A33; height: 43px; cursor: pointer;">
+    <img class="pt-1 ml-1" style="float: left" width="40" src="./assets/img/metamask-light.png">
+    <p class="pt-2" style="font-weight: 500; color: white; font-size: 14px;">Enter with
+        Metamask</p>
+</div>
---
+++ src/pages/content/login.svelte
@@ -0,0 +1,30 @@
+<script>
+    import MetaButton from '../../components/metamask_button.svelte';
+    import Icon from 'fa-svelte';
+    import {faArrowRight, faArrowLeft} from '@fortawesome/free-solid-svg-icons';
+</script>
+
+
+<section class="section">
+    <!--menu--->
+
+    <!--end menu--->
+    <div class="section-body" style="padding-right: 30px; padding-left: 30px">
+        <div class="col-lg-3 col-md-3 col-sm-3 col-12 text-center mt-5" style="margin: 0 auto;">
+            <div class="card">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        <div class="row">
+                            <div class="col">
+                                <h4 style="font-weight: 500; color: #262626; text-align: start;">Login</h4>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <MetaButton />
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
