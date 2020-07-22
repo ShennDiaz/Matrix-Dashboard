ad3e5dd8995f12eecd9563abbbe0a47ed2ff16bd
Miguel <miguel@solus.local>
2020-07-22 12:11:50 -0500

    Up

---
+++ src/components/qr_modal.svelte
@@ -0,0 +1,22 @@
+<script>
+    import QR from './qr_item.svelte';
+
+    export let address;
+</script>
+
+<div class="modal fade" id="qr" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header" style="background-color: white;">
+                <p style="color: rgb(118, 210, 117); font-weight: 600; font-size: 22px;" class="modal-title pb-3" id="exampleModalLongTitle">My Address</p>
+                <button style="color: white;" type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body" style="background-color: #F8F8F8;">
+                <QR codeValue="{address}" squareSize="{150}"/>
+                <p class="mt-3 text-center" style="font-size: 14px;">Matrix wallet: {address}</p>
+            </div>
+        </div>
+    </div>
+</div>
